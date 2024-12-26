a19fcdedac5e701861592c01003c29d33855aa85
infosetGang <kangyoonkyung@infoset.co.kr>
2024-12-26 13:11:57 +0900

    c타입 강좌목록 가로 작업 완료

--- src/components/kiosk/c/course/index.tsx
+++ src/components/kiosk/c/course/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import {
-  Column,
   Container,
   CustomSwiper,
   ItemWrapper,
   SlideItem,
   Wrapper,
+  Column,
+  ContainerX,
+  CustomSwiperX,
+  ItemWrapperX,
+  SlideItemX,
+  WrapperX,
+  ColumnX,
 } from './index.styled';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import { SwiperSlide } from 'swiper/react';
 import { course_list } from '../../../../constants/course.constants';
-import { Pagination } from 'swiper/modules';
+import { Pagination, Navigation } from 'swiper/modules';
 import { useSelectedItemStore } from '../../../../store/selected-item.store';
 import { useOrientationStore } from '../../../../store/landscape-mode.store';
 
@@ -22,7 +29,111 @@ const Course = () => {
   return (
     <>
       {isLandscape ? (
-        <div>강좌안내 가로 모드</div>
+        <ContainerX>
+          <button className={'swiper-button-prev'}>Prev</button>
+          <button className={'swiper-button-next'}>Next</button>
+          <CustomSwiperX
+            slidesPerView={1}
+            slidesPerGroup={1}
+            modules={[Navigation]}
+            spaceBetween={5}
+            navigation={{
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            }}
+            loop
+          >
+            <SwiperSlide>
+              <ItemWrapperX>
+                {course_list?.slice(0, 5).map((item, index) => (
+                  <SlideItemX key={index} onClick={() => toggleSelectedItem(8)}>
+                    <WrapperX>
+                      <div className={`${'badge'} ${item.className}`}>
+                        {item.status}
+                      </div>
+                      <div>
+                        <div className={'title'}>{item.title}</div>
+                        <WrapperX>
+                          <WrapperX>
+                            <div className={'label'}>접수기간</div>
+                            <div className={'value'}>{item.date}</div>
+                          </WrapperX>
+                          <WrapperX>
+                            <div className={'label'}>수강대상</div>
+                            <div className={'value'}>{item.user}</div>
+                          </WrapperX>
+                        </WrapperX>
+                      </div>
+                    </WrapperX>
+                    <WrapperX>
+                      <ColumnX className={'recruit'}>
+                        <div className={'count_label'}>모집인원</div>
+                        <div className={'count'}>
+                          <span className={'highlight'}>{item.recruited}</span>
+                          <span className={'dash'}>/</span>
+                          {item.recruit}
+                        </div>
+                      </ColumnX>
+                      <ColumnX className={'candidate'}>
+                        <div className={'count_label'}>후보인원</div>
+                        <div className={'count'}>
+                          <span className={'highlight'}>{item.became}</span>
+                          <span className={'dash'}>/</span>
+                          {item.candidate}
+                        </div>
+                      </ColumnX>
+                    </WrapperX>
+                  </SlideItemX>
+                ))}
+              </ItemWrapperX>
+            </SwiperSlide>
+            <SwiperSlide>
+              <ItemWrapperX>
+                {course_list?.slice(5, 10).map((item, index) => (
+                  <SlideItemX key={index} onClick={() => toggleSelectedItem(8)}>
+                    <WrapperX>
+                      <div className={`${'badge'} ${item.className}`}>
+                        {item.status}
+                      </div>
+                      <div>
+                        <div className={'title'}>{item.title}</div>
+                        <WrapperX>
+                          <WrapperX>
+                            <div className={'label'}>접수기간</div>
+                            <div className={'value'}>{item.date}</div>
+                          </WrapperX>
+                          <WrapperX>
+                            <div className={'label'}>수강대상</div>
+                            <div className={'value'}>{item.user}</div>
+                          </WrapperX>
+                        </WrapperX>
+                      </div>
+                    </WrapperX>
+
+                    <WrapperX>
+                      <ColumnX className={'recruit'}>
+                        <div className={'count_label'}>모집인원</div>
+                        <div className={'count'}>
+                          <span className={'highlight'}>{item.recruited}</span>
+                          <span className={'dash'}>/</span>
+                          {item.recruit}
+                        </div>
+                      </ColumnX>
+                      <ColumnX className={'candidate'}>
+                        <div className={'count_label'}>후보인원</div>
+                        <div className={'count'}>
+                          <span className={'highlight'}>{item.became}</span>
+                          <span className={'dash'}>/</span>
+                          {item.candidate}
+                        </div>
+                      </ColumnX>
+                    </WrapperX>
+                  </SlideItemX>
+                ))}
+              </ItemWrapperX>
+            </SwiperSlide>
+          </CustomSwiperX>
+        </ContainerX>
       ) : (
         <Container>
           <CustomSwiper
