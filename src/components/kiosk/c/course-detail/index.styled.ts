c3a801c81cc035f927bc990bd8f889efae25588d
infosetGang <kangyoonkyung@infoset.co.kr>
2024-12-26 13:16:56 +0900

    c타입 강좌정보 가로 작업 완료

--- src/components/kiosk/c/course-detail/index.styled.ts
+++ src/components/kiosk/c/course-detail/index.styled.ts
@@ -1,5 +1,6 @@
 import { styled } from '@mui/material';
 import background from '../../../../assets/img/kiosk_c_y_course_detail_bg.svg';
+import backgroundX from '../../../../assets/img/kiosk_c_x_course_detail_bg.svg';
 
 const Container = styled('div')(() => ({
   width: '23.7275rem',
@@ -36,4 +37,39 @@ const Button = styled('div')(() => ({
   borderRadius: '0.43938rem',
 }));
 
-export { Container, Button };
+const ContainerX = styled('div')(() => ({
+  width: '75rem',
+  height: '42.1875rem',
+  background: `url(${backgroundX}) center center no-repeat`,
+  backgroundSize: 'cover',
+  position: 'relative',
+  opacity: 0,
+  animation: 'fadeIn 1s ease-out forwards',
+  '@keyframes fadeIn': {
+    '0%': {
+      opacity: 0,
+    },
+    '100%': {
+      opacity: 1,
+    },
+  },
+}));
+
+const ButtonX = styled('div')(() => ({
+  width: '35.15625rem',
+  height: '2.73438rem',
+  marginTop: '35.21rem',
+  marginLeft: '23.05rem',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  cursor: 'pointer',
+  color: '#FFFFFF',
+  fontSize: '0.85938rem',
+  fontWeight: 700,
+  fontFamily: 'Pretendard',
+  background: '#2E80EE',
+  borderRadius: '0.78125rem',
+}));
+
+export { Container, Button, ContainerX, ButtonX };
