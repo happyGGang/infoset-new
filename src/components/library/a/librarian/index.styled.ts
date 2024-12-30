73c303369db3b3076fc74cf454f43dd17771e6f4
Yunkyung Kang <96277834+happyGGang@users.noreply.github.com>
2024-12-30 13:27:31 +0000

    [feat] 라이브러리보드 a타입 사서추천도서 작업중

---
+++ src/components/library/a/librarian/index.styled.ts
@@ -0,0 +1,131 @@
+import { styled } from '@mui/material';
+import background from '../../../../assets/img/library_a_librarian_bg.svg';
+import { Swiper } from 'swiper/react';
+
+const Container = styled('div')(() => ({
+    width: '23.7275rem',
+    height: '42.18225rem',
+    background: `url(${background}) center center no-repeat`,
+    backgroundSize: 'cover',
+    alignItems: 'center',
+    flexDirection: 'column',
+    position: 'relative',
+    '.animate': {
+      animation: 'float-up 0.5s ease-out forwards',
+    },
+    '@keyframes float-up': {
+      '0%': {
+        opacity: 0,
+      },
+      '100%': {
+        opacity: 1,
+      },
+    },
+    opacity: 0,
+    animation: 'fadeIn 1s ease-out forwards',
+    '@keyframes fadeIn': {
+      '0%': {
+        opacity: 0,
+      },
+      '100%': {
+        opacity: 1,
+      },
+    },
+    '.time_date_wrapper': {
+      zIndex: 1,
+      position: 'absolute',
+      display: 'flex',
+      flexDirection: 'column',
+      top: '1.32rem',
+      right: '2.2rem',
+    },
+  }));
+
+  const Time = styled('div')(() => ({
+    color: '#FFF',
+    fontFamily: 'Century Gothic',
+    fontSize: '0.83506rem',
+    fontWeight: 400,
+    lineHeight: 'normal',
+    textAlign: 'center',
+  }));
+  
+  const Date = styled('div')(() => ({
+    color: '#FFF',
+    fontFamily: 'Pretendard',
+    fontSize: '0.30763rem',
+    fontWeight: 400,
+    lineHeight: 'normal',
+    textAlign: 'center',
+  }));
+
+  const CustomSwiper = styled(Swiper)(() => ({
+    width: '19.89414rem',
+    height: '32.39502rem',
+    paddingLeft: '0.05rem',
+    margin: '4.39rem 0 0 2.42rem',
+    '.swiper-pagination': {
+      bottom: '0',
+      left: '-0.5rem',
+    },
+    '.swiper-pagination-bullet': {
+      opacity: '1',
+      width: '0.26363rem',
+      height: '0.26363rem',
+      background: '#8B95A1',
+      borderRadius: '50%',
+      transition: 'background-color 0.3s ease, transform 0.3s ease',
+    },
+    '.swiper-pagination-bullet-active': {
+      background: '#FFFFFF',
+    },
+  }));
+  
+  const Grid = styled('div')(() => ({
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridRowGap: '1.35rem',
+    gridColumnGap: '0.5rem',
+  }));
+  
+  const SlideItem = styled('div')(() => ({
+    display: 'flex',
+    flexDirection: 'column',
+    cursor: 'pointer',
+    '& img': {
+      width: '5.27281rem',
+      height: '7.60156rem',
+      borderRadius: '0.26363rem',
+      boxShadow: '0px 1.758px 5.273px 0px rgba(25, 33, 61, 0.20)',
+      marginBottom: '0.44rem',
+    },
+    '& .title': {
+      color: '#191F28',
+      fontFamily: 'Pretendard',
+      fontWeight: 500,
+      lineHeight: '140%',
+      letterSpacing: '-0.01319rem',
+      fontSize: '0.52725rem',
+      marginBottom: '0.09rem',
+      padding: '0 0.09rem 0 0.09rem',
+      width: '5.27281rem',
+      textOverflow: 'ellipsis',
+      overflow: 'hidden',
+      whiteSpace: 'nowrap',
+    },
+    '& .writer': {
+      color: '#4E5968',
+      fontFamily: 'Pretendard',
+      fontWeight: 400,
+      letterSpacing: '-0.011rem',
+      fontSize: '0.43938rem',
+      marginBottom: '0.09rem',
+      padding: '0 0.09rem 0 0.09rem',
+      width: '5.27281rem',
+      textOverflow: 'ellipsis',
+      overflow: 'hidden',
+      whiteSpace: 'nowrap',
+    },
+  }));
+
+  export {Container,Date,Time, CustomSwiper, Grid, .SlideItem}
--- src/components/library/a/librarian/index.tsx
+++ src/components/library/a/librarian/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import {
+  Container,
+  Date,
+  Time,
+} from './index.styled';
+import { getCurrentDate, getCurrentTime } from '../../../../util/date-time';
 
 const Librarian = () => {
-  return <div></div>;
+  return <Container>
+  <div className={'time_date_wrapper'}>
+    <Time>{getCurrentTime()}</Time>
+    <Date>{getCurrentDate()}</Date>
+  </div>
+  
+</Container>;
 };
 
 export default Librarian;
