0ec3b6f2eabd9bf753524189ffe899e18bd0ef9b
infosetGang <kangyoonkyung@infoset.co.kr>
2025-01-20 17:39:03 +0900

    라이브러리보드 c 대출반납, 인기도서, 사서추천 작업 완료

--- src/components/library/c/best/index.tsx
+++ src/components/library/c/best/index.tsx
@@ -1,53 +1,36 @@
 import React from 'react';
-import {
-  Container,
-  CustomSwiper,
-  Grid,
-  SlideItem,
-  Date,
-  Time,
-} from './index.styled';
-import { Autoplay, Pagination } from 'swiper/modules';
+import { Container, CustomSwiper, Grid, SlideItem } from './index.styled';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
 import { SwiperSlide } from 'swiper/react';
 import { book_list } from '../../../../constants/book.constants';
-import { getCurrentDate, getCurrentTime } from '../../../../util/date-time';
+import { useSelectedItemStore } from '../../../../store/selected-item.store';
 
 const Best = () => {
+  const { toggleSelectedItem } = useSelectedItemStore();
+
   return (
     <Container>
-      <div className={'time_date_wrapper'}>
-        <Time>{getCurrentTime()}</Time>
-        <Date>{getCurrentDate()}</Date>
-      </div>
       <CustomSwiper
         slidesPerView={1}
         slidesPerGroup={1}
         pagination
         spaceBetween={5}
-        modules={[Pagination, Autoplay]}
+        modules={[Pagination]}
         loop
-        autoplay={{ delay: 10000 }}
-        allowTouchMove={false}
       >
         <SwiperSlide>
           <Grid>
             {book_list?.slice(0, 5).map((item, index) => (
-              <SlideItem key={index}>
-                <div className={'badge'}>{index + 1}위</div>
-                <img src={item.img} alt="" />
-                <div>
-                  <div className={'title'}>{item.title}</div>
-                  <div className={'writer'}>{item.writer}</div>
-                  <div className={'publisher'}>
-                    {item.publisher}ㆍ{item.date}
-                  </div>
-                  <div className={'detail'}>
-                    <span>소창위치</span>2F 종합자료실
-                  </div>
-                  <div className={'detail'}>
-                    <span>청구기호</span>유아813.8 신211o
-                  </div>
+              <SlideItem key={index} onClick={() => toggleSelectedItem(6)}>
+                <div className={'badge'}>
+                  {(index + 1).toString().padStart(2, '0')}
                 </div>
+                <img src={item.img} alt="" />
+                <div className={'title'}>{item.title}</div>
+                <div className={'writer'}>{item.writer}</div>
+                <div className={'writer'}>{item.publisher}</div>
               </SlideItem>
             ))}
           </Grid>
@@ -55,22 +38,14 @@ const Best = () => {
         <SwiperSlide>
           <Grid>
             {book_list?.slice(6, 11).map((item, index) => (
-              <SlideItem key={index}>
-                <div className={'badge'}>{index + 6}위</div>
-                <img src={item.img} alt="" />
-                <div>
-                  <div className={'title'}>{item.title}</div>
-                  <div className={'writer'}>{item.writer}</div>
-                  <div className={'publisher'}>
-                    {item.publisher}ㆍ{item.date}
-                  </div>
-                  <div className={'detail'}>
-                    <span>소창위치</span>2F 종합자료실
-                  </div>
-                  <div className={'detail'}>
-                    <span>청구기호</span>유아813.8 신211o
-                  </div>
+              <SlideItem key={index} onClick={() => toggleSelectedItem(6)}>
+                <div className={'badge'}>
+                  {(index + 6).toString().padStart(2, '0')}
                 </div>
+                <img src={item.img} alt="" />
+                <div className={'title'}>{item.title}</div>
+                <div className={'writer'}>{item.writer}</div>
+                <div className={'writer'}>{item.publisher}</div>
               </SlideItem>
             ))}
           </Grid>
--- src/components/library/c/return/index.styled.ts
+++ src/components/library/c/return/index.styled.ts
@@ -1,5 +1,5 @@
 import { styled } from '@mui/material';
-import background from '../../../../assets/img/library_a_return_bg.svg';
+import background from '../../../../assets/img/library_c_return_bg.svg';
 
 const Container = styled('div')(() => ({
   width: '23.7275rem',
@@ -17,32 +17,6 @@ const Container = styled('div')(() => ({
       opacity: 1,
     },
   },
-  '.time_date_wrapper': {
-    zIndex: 1,
-    position: 'absolute',
-    display: 'flex',
-    flexDirection: 'column',
-    top: '1.32rem',
-    right: '2.2rem',
-  },
 }));
 
-const Time = styled('div')(() => ({
-  color: '#FFF',
-  fontFamily: 'Century Gothic',
-  fontSize: '0.83506rem',
-  fontWeight: 400,
-  lineHeight: 'normal',
-  textAlign: 'center',
-}));
-
-const Date = styled('div')(() => ({
-  color: '#FFF',
-  fontFamily: 'Pretendard',
-  fontSize: '0.30763rem',
-  fontWeight: 400,
-  lineHeight: 'normal',
-  textAlign: 'center',
-}));
-
-export { Container, Time, Date };
+export { Container };
