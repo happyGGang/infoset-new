ac8e0941be9757c279e711fc9bedf81ffe1db26b
infosetGang <kangyoonkyung@infoset.co.kr>
2025-01-21 10:48:58 +0900

    라이브러리보드 c 타입 생활정보 작업 완료

--- src/components/library/c/living/index.styled.ts
+++ src/components/library/c/living/index.styled.ts
@@ -1,5 +1,5 @@
 import { styled } from '@mui/material';
-import background from '../../../../assets/img/library_a_living_bg.svg';
+import background from '../../../../assets/img/library_c_living_bg.svg';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 const Container = styled('div')(() => ({
@@ -18,69 +18,21 @@ const Container = styled('div')(() => ({
       opacity: 1,
     },
   },
-  '.time_date_wrapper': {
-    zIndex: 1,
-    position: 'absolute',
-    display: 'flex',
-    flexDirection: 'column',
-    top: '1.32rem',
-    right: '2.2rem',
-  },
-  '.swiper_container': {
-    width: '23.733rem',
-    height: '6.5925rem',
-    background: 'rgba(0, 0, 0, 0.40)',
-    backdropFilter: 'blur(7.031999588012695px)',
-    display: 'flex',
-    flexDirection: 'column',
-    gap: '0.66rem',
-    marginTop: '34rem',
-  },
-  '.badge': {
-    width: '1.9rem',
-    height: '1.0rem',
-    borderRadius: '0.26rem',
-    background: '#333D4B',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: '#FFF',
-    fontSize: '0.52738rem',
-    lineHeight: 'normal',
-    letterSpacing: '-0.01319rem',
-    marginTop: '1rem',
-    marginLeft: '2.2rem',
-  },
-}));
-
-const Time = styled('div')(() => ({
-  color: '#FFF',
-  fontFamily: 'Century Gothic',
-  fontSize: '0.83506rem',
-  fontWeight: 400,
-  lineHeight: 'normal',
-  textAlign: 'center',
-}));
-
-const Date = styled('div')(() => ({
-  color: '#FFF',
-  fontFamily: 'Pretendard',
-  fontSize: '0.30763rem',
-  fontWeight: 400,
-  lineHeight: 'normal',
-  textAlign: 'center',
 }));
 
 const CustomSwiper = styled(Swiper)(() => ({
-  width: '19.338rem',
-  height: '1.79rem',
+  width: '17.1405rem',
+  height: '8.379rem',
+  marginTop: '30rem',
+  marginLeft: '3.3rem',
 }));
 
 const CustomSwiperSlide = styled(SwiperSlide)(() => ({
   display: 'flex',
-  flexDirection: 'column',
+  gap: '0.03rem',
+  color: '#FFF',
   '& .label': {
-    color: '#8B95A1',
+    color: '#ADB5BD',
     fontSize: '0.52738rem',
     fontWeight: 400,
     lineHeight: '140%',
@@ -93,11 +45,13 @@ const CustomSwiperSlide = styled(SwiperSlide)(() => ({
     fontWeight: 400,
     lineHeight: '140%',
     letterSpacing: '-0.0165rem',
-    width: '19.338rem',
     textOverflow: 'ellipsis',
     overflow: 'hidden',
-    whiteSpace: 'nowrap',
+    WebkitBoxOrient: 'vertical',
+    WebkitLineClamp: '4',
+    display: '-webkit-box',
+    height: '1.875rem',
   },
 }));
 
-export { Container, Time, Date, CustomSwiperSlide, CustomSwiper };
+export { Container, CustomSwiperSlide, CustomSwiper };
--- src/components/library/c/living/index.tsx
+++ src/components/library/c/living/index.tsx
@@ -1,41 +1,29 @@
 import React from 'react';
-import {
-  Container,
-  CustomSwiper,
-  CustomSwiperSlide,
-  Date,
-  Time,
-} from './index.styled';
-import { getCurrentDate, getCurrentTime } from '../../../../util/date-time';
+import { Container, CustomSwiper, CustomSwiperSlide } from './index.styled';
 import { news_list } from '../../../../constants/news.constants';
 import { Autoplay } from 'swiper/modules';
 
 const Living = () => {
   return (
     <Container>
-      <div className={'time_date_wrapper'}>
-        <Time>{getCurrentTime()}</Time>
-        <Date>{getCurrentDate()}</Date>
-      </div>
-      <div className={'swiper_container'}>
-        <div className={'badge'}>뉴스</div>
-        <CustomSwiper
-          slidesPerView={1}
-          direction={'vertical'}
-          modules={[Autoplay]}
-          spaceBetween={5}
-          loop
-          allowTouchMove={false}
-          autoplay={{ delay: 5000 }}
-        >
-          {news_list?.map((item, index) => (
-            <CustomSwiperSlide key={index}>
-              <div className={'label'}>{item.date}</div>
+      <CustomSwiper
+        slidesPerView={3}
+        direction={'vertical'}
+        modules={[Autoplay]}
+        loop
+        allowTouchMove={false}
+        autoplay={{ delay: 5000 }}
+      >
+        {news_list?.map((item, index) => (
+          <CustomSwiperSlide key={index}>
+            <div>ㆍ </div>
+            <div>
               <div className={'value'}>{item.title}</div>
-            </CustomSwiperSlide>
-          ))}
-        </CustomSwiper>
-      </div>
+              <div className={'label'}>{item.date}</div>
+            </div>
+          </CustomSwiperSlide>
+        ))}
+      </CustomSwiper>
     </Container>
   );
 };
