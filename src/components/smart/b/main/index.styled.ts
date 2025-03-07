93aa7c34e7da196e818ea2554927a574f34e0175
infosetGang <kangyoonkyung@infoset.co.kr>
2025-03-07 13:56:17 +0900

    스마트추천도서 b 메인 작업 완료

--- src/components/smart/b/main/index.styled.ts
+++ src/components/smart/b/main/index.styled.ts
@@ -1,6 +1,6 @@
 import { styled } from '@mui/material';
-import background from '../../../../assets/img/smart_a_y_main.svg';
-import backgroundX from '../../../../assets/img/smart_a_x_main.svg';
+import background from '../../../../assets/img/smart_b_y_main_bg.svg';
+import backgroundX from '../../../../assets/img/smart_b_x_main_bg.svg';
 
 const Container = styled('div')(() => ({
   width: '23.7275rem',
@@ -12,14 +12,42 @@ const Container = styled('div')(() => ({
   position: 'relative',
   opacity: 0,
   animation: 'fadeIn 1s ease-out forwards',
+  overflow: 'hidden',
 
   '@keyframes fadeIn': {
-    '0%': {
-      opacity: 0,
-    },
-    '100%': {
-      opacity: 1,
-    },
+    '0%': { opacity: 0 },
+    '100%': { opacity: 1 },
+  },
+
+  '@keyframes floating': {
+    '0%': { transform: 'translateY(0)' },
+    '50%': { transform: 'translateY(-10px)' }, // 위로 10px 이동
+    '100%': { transform: 'translateY(0)' },
+  },
+
+  '& .character': {
+    width: '22.56181rem',
+    height: '18.46044rem',
+    position: 'absolute',
+    top: '8.91rem',
+    left: '-1.63rem',
+    animation: 'floating 2.5s ease-in-out infinite',
+  },
+  '& .book': {
+    width: '5.13438rem',
+    height: '6.40988rem',
+    position: 'absolute',
+    top: '6.2rem',
+    right: '3.67rem',
+    animation: 'floating 3s ease-in-out infinite',
+  },
+  '& .search': {
+    width: '4.573rem',
+    height: '4.77088rem',
+    position: 'absolute',
+    top: '20.51rem',
+    right: '3.22rem',
+    animation: 'floating 2s ease-in-out infinite',
   },
 }));
 
@@ -32,6 +60,7 @@ const ContainerX = styled('div')(() => ({
   flexDirection: 'column',
   position: 'relative',
   opacity: 0,
+  overflow: 'hidden',
   animation: 'fadeIn 1s ease-out forwards',
   '@keyframes fadeIn': {
     '0%': {
@@ -40,6 +69,37 @@ const ContainerX = styled('div')(() => ({
     '100%': {
       opacity: 1,
     },
+  },
+
+  '@keyframes floating': {
+    '0%': { transform: 'translateY(0)' },
+    '50%': { transform: 'translateY(-10px)' }, // 위로 10px 이동
+    '100%': { transform: 'translateY(0)' },
+  },
+
+  '& .character': {
+    width: '33.25rem',
+    height: '27rem',
+    position: 'absolute',
+    top: '11.29rem',
+    left: '-4.22rem',
+    animation: 'floating 2.5s ease-in-out infinite',
+  },
+  '& .book': {
+    width: '9.36719rem',
+    height: '10.30081rem',
+    position: 'absolute',
+    top: '7.48rem',
+    left: '22.85rem',
+    animation: 'floating 3s ease-in-out infinite',
+  },
+  '& .search': {
+    width: '6.41406rem',
+    height: '6.74613rem',
+    position: 'absolute',
+    top: '28.44rem',
+    left: '25.98rem',
+    animation: 'floating 2s ease-in-out infinite',
   },
 }));
 
--- src/components/smart/b/main/index.tsx
+++ src/components/smart/b/main/index.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Container, ContainerX } from './index.styled';
 import { useOrientationStore } from '../../../../store/landscape-mode.store';
+import character from '../../../../assets/img/smart_character.svg';
+import book from '../../../../assets/img/smart_book.svg';
+import search from '../../../../assets/img/smart_search.svg';
 
 const Main = () => {
   const { isLandscape } = useOrientationStore();
 
   return (
-    <>{isLandscape ? <ContainerX></ContainerX> : <Container></Container>}</>
+    <>
+      {isLandscape ? (
+        <ContainerX>
+          <img className={'character'} src={character} alt="" />
+          <img className={'book'} src={book} alt="" />
+          <img className={'search'} src={search} alt="" />
+        </ContainerX>
+      ) : (
+        <Container>
+          <img className={'character'} src={character} alt="" />
+          <img className={'book'} src={book} alt="" />
+          <img className={'search'} src={search} alt="" />
+        </Container>
+      )}
+    </>
   );
 };
 
